e97b0160f4cd7e4a93232c8d96cbcfe096b7629a
OfekCP <ofekzvulunov@gmail.com>
2024-04-26 15:54:53 +0300

    update

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default {
-  // other config options...
-  server: {
-    proxy: {
-      '/auth': `${import.meta.env.VITE_API_URL}`,
-      '/api/travel':  `${import.meta.env.VITE_API_URL}`,
-      
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+})
